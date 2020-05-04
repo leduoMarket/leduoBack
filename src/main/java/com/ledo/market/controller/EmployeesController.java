ec67fe354883774bebbf73ff62a86c3c27617424
horizon <1713113814@qq.com>
2020-05-05 01:10:33 +0800

    add emloyee is ok!

--- src/main/java/com/ledo/market/controller/EmployeesController.java
+++ src/main/java/com/ledo/market/controller/EmployeesController.java
@@ -1,18 +1,11 @@
 package com.ledo.market.controller;
-
 import com.ledo.market.entity.Employee;
 import com.ledo.market.mapper.EmployeeMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ledo.market.result.StatusCodeResult;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
-import javax.sql.DataSource;
-import java.util.Collection;
 import java.util.List;
-
 /**
  * @author 王梦琼
  */
@@ -24,8 +17,20 @@ public class EmployeesController {
     @GetMapping("/emps")
     public List<Employee> list(Model model){
         return employeeMapper.selectAll();
-//        List<Employee> employees = employeeMapper.selectAll();
-//        model.addAttribute("emps",employees);
-//        return employees;
     }
+    @CrossOrigin
+    @PostMapping("/addemp")
+    @ResponseBody
+    public StatusCodeResult addemp(@RequestBody Employee reqemp){
+        System.out.print(reqemp.getEid());
+        System.out.println(reqemp.getEname());
+        System.out.println(employeeMapper.insert(reqemp));
+        return new StatusCodeResult(200);
+    }
+//    @CrossOrigin
+//    @GetMapping("/delemp/{empId}")
+//    public StatusCodeResult delemp(@PathVariable int empId){
+//        System.out.println(employeeMapper.delete(empId));
+//        return new StatusCodeResult(200);
+//    }
 }
