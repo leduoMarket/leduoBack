074e06a2a631b73d7ff893bda5ce79680a72f6b2
grx <1115311378@qq.com>
2020-05-14 16:04:11 +0800

    grx commit 514

--- src/main/java/com/ledo/market/controller/UserController.java
+++ src/main/java/com/ledo/market/controller/UserController.java
@@ -6,11 +6,21 @@ import com.ledo.market.result.StatusCodeResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class UserController {
     @Resource
     UserMapper usermapper;
+
+    //这个是显示在employee界面上的所有数据
+    @CrossOrigin
+    @GetMapping("/admin/emps")
+    public List<User> list(){
+        return usermapper.selectAll();
+    }
+
+    //这个是写在register页面上的注册，向user表中新增数据
     @CrossOrigin
     @PostMapping("/addregister")
     @ResponseBody
@@ -25,4 +35,49 @@ public class UserController {
         System.out.println(usermapper.insert(requser));
         return new StatusCodeResult(200);
     }
+
+    //这个是在employyee界面进行更新操作
+    @CrossOrigin
+    @PostMapping("/editemp")
+    @ResponseBody
+    public StatusCodeResult updateuser(@RequestBody User requser) {
+        System.out.println("UID:" + requser.getUserName());
+        System.out.println("UID:" + requser.getUid());
+        System.out.println(usermapper.updateByPrimaryKey(requser));
+        return new StatusCodeResult(200);
+    }
+
+    //这个是在employee界面上删除数据
+    @CrossOrigin
+    @DeleteMapping("/delemp")
+    public StatusCodeResult delemp(@RequestParam(value = "empId") String empId){
+        System.out.println("empID:"+empId);
+        System.out.println(usermapper.delete(empId));
+        return new StatusCodeResult(200);
+    }
+    //这个是在loginuser界面修改密码操作
+    @CrossOrigin
+    @PostMapping("/updatepwd")
+    @ResponseBody
+    public StatusCodeResult updateuser(@RequestParam(value = "uid") String uid,@RequestParam(value = "pwd1") String pwd1,@RequestParam(value = "pwd2") String pwd2) {
+        System.out.println("UID:" + uid);
+        System.out.println("oldpwd:" + pwd1);
+        System.out.println("newpwd:" + pwd2);
+        System.out.println(usermapper.getUserByUid(uid,pwd1,pwd2));
+        return new StatusCodeResult(200);
+    }
+
+    //这个是向loginuser界面传入信息
+    @CrossOrigin
+    @GetMapping("/getUserInfo")
+    public User selectByPrimaryKey(@RequestParam(value = "uid") String uid){
+        User s = usermapper.selectByPrimaryKey(uid);
+        if(s!=null){
+            System.out.println("uid"+s.getUid());
+            System.out.println("password"+s.getPassword());
+            return s;
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/ledo/market/mapper/UserMapper.java
+++ src/main/java/com/ledo/market/mapper/UserMapper.java
@@ -3,11 +3,19 @@ package com.ledo.market.mapper;
 import com.ledo.market.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @author 王梦琼
  */
 @Mapper
 public interface UserMapper {
-    public User getUserByUid(String uid);
+    public User getUserByUid(String uid,String pwd1,String pwd2);
     int insert(User record);
+    int updateByPrimaryKey(User record);
+    List<User> selectAll();
+    User selectByPrimaryKey(String uid);
+    String delete(String uid);
 }
+
+
